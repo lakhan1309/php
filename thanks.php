91f91e63c9190c211c6a6e723cbca691a7be5df1
Varun Kumar <vrnkmr55@gmail.com>
2016-11-21 00:24:54 +0530

    Add files via upload

---
+++ thanks.php
@@ -0,0 +1,32 @@
+<?php header('Refresh:4; URL=http://localhost/master/index.php');?>
+<html>
+<head>
+
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+  <title>Thank You</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Font Awesome CSS-->
+  <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+
+  <!-- Custom styles for this template -->
+  <link href="css/main.css" rel="stylesheet">
+  <script src="jcubic/js/jquery-1.7.1.min.js"></script>
+  <script src="jcubic/js/jquery.mousewheel-min.js"></script>
+  <script src="jcubic/js/jquery.terminal.min.js"></script>
+  <script src="js/smoothscroll.js"></script>
+  <link href="jcubic/css/jquery.terminal.css" rel="stylesheet"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+            <br><br>  <h3 class="masthead-brand"><i class="fa fa-laptop" aria-hidden="true"></i> CompilePad</h3><br><br>
+     <h6 class="featurette-heading">Thanks for your submission.<span class="text-muted">We'll get back to you shortly.</span></h6>
+<br><br>
+          <h3>Redirecting to <i class="fa fa-laptop" aria-hidden="true">Compile pad</h3></i> <i class="fa fa-circle-o-notch fa-spin" style="font-size:24px"></i>
+</body>
+</html>
