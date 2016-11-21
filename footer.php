a26e955657304af880dbc4ba9f2fd90c590f0d82
lakhan khandelwal <lakhan.khandelwal10@gmail.com>
2016-11-21 18:31:02 +0530

    lakhan

--- footer.php
+++ footer.php
@@ -63,10 +63,10 @@
 
 				<div class="footer-icons">
 
-					<a href="https://www.facebook.com/compilepad"><i class="fa fa-facebook"></i></a>
-					<a href="https://twitter.com/compile_pad"><i class="fa fa-twitter"></i></a>
-					<!--<a href="#"><i class="fa fa-linkedin"></i></a>-->
-					<a href="https://github.com/compilepad/Minor-Project.git"><i class="fa fa-github"></i></a>
+					<a href="https://www.facebook.com/compilepad" target="_blank"><i class="fa fa-facebook"></i></a>
+					<a href="https://twitter.com/compile_pad" target="_blank"><i class="fa fa-twitter"></i></a>
+					<!--<a href="#"><i class="fa fa-linkedin" target="_blank"></i></a>-->
+					<a href="https://github.com/compilepad/Minor-Project.git" target="_blank"><i class="fa fa-github"></i></a>
 
 				</div>
 
